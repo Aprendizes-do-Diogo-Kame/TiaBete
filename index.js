189e66a5467d3030050c4f2aa46a00a300101372
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2023-12-15 11:18:58 -0300

    tiabete: first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+var express = require('express')
+  , bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+app.get('/webhook', function(req, res) {
+  if (
+    req.query['hub.mode'] == 'subscribe' &&
+    req.query['hub.verify_token'] == FB_VERIFICATION_TOKEN
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post("/webhook", function (request, response) {
+  console.log('Incoming webhook: ' + JSON.stringify(request.body));
+  response.sendStatus(200);
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
