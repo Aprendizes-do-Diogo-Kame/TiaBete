04aae82220fd1cafd0750cdabf9f3ce6b3dbce2d
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2023-12-15 15:32:33 -0300

    tiabete: tratamento do tipo de texto

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/webhook', function(req, res) {
     req.query['hub.verify_token'] == process.env.FB_VERIFICATION_TOKEN
   ) {
     res.send(req.query['hub.challenge']);
-    console.log("Facebook verificou a URL")
+    console.log("Facebook verificou a URL");
   } else {
     res.sendStatus(400);
   }
@@ -27,6 +27,24 @@ app.get('/webhook', function(req, res) {
 app.post("/webhook", function (request, response) {
   console.log('Incoming webhook: ' + JSON.stringify(request.body));
   response.sendStatus(200);
+  if (
+	request.body.entry &&
+	request.body.entry[0].changes &&
+	request.body.entry[0].changes[0] &&
+	request.body.entry[0].changes[0].value.messages &&
+	request.body.entry[0].changes[0].value.messages[0]
+) {
+    let messageType = request.body.entry[0].changes[0].value.messages[0].type;
+    let messageContent = request.body.entry[0].changes[0].value.messages[0].text.body;
+    let messageFrom = request.body.entry[0].changes[0].value.messages[0].text.body;
+    if(messageType == "text"){
+        console.log(messageContent);
+        console.log(messageFrom);
+    } 
+    response.sendStatus(200);
+} else {
+	response.sendStatus(400);
+}
 });
 
 var listener = app.listen(process.env.PORT, function () {
