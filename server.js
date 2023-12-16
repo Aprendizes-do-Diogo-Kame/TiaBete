7bb9d71a27807633c3a7b7664e95e8891b531458
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2023-12-16 15:51:54 -0300

    adiciona awaits nas funções assincronas

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/webhook', function(req, res) {
   }
 });
 
-app.post("/webhook", function (request, response) {
+app.post("/webhook", async function (request, response) {
   console.log('Incoming webhook: ' + JSON.stringify(request.body));
   if (
 	request.body.entry &&
@@ -45,11 +45,11 @@ app.post("/webhook", function (request, response) {
       if(messageType == "text"){
         let messageContent = request.body.entry[0].changes[0].value.messages[0].text.body;
         console.log(messageContent);
-        msgText = chat.chatGptService.categorize(messageTimeStamp, messageContent)
+        msgText = await chat.chatGptService.categorize(messageTimeStamp, messageContent)
         console.log(msgText);
         chat.text.send(ourNumberId, messageFrom, msgText);
       } else if(messageType == "audio"){
-        msgText = chat.chatGptService.categorize(messageTimeStamp, messageContent);
+        msgText = await chat.chatGptService.categorize(messageTimeStamp, messageContent);
         chat.text.send(ourNumberId, messageFrom, msgText);
       } else {
         console.log("API inconsistente")
