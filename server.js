9500e183f2ec96822e8a0bbbb73014f7084bade7
ArthurFaria <arthurmedeirosfa@gmail.com>
2023-12-16 21:05:57 -0300

    feat: :sparkles: inicio da transcrição de aúdio pelo chatbot

--- server.js
+++ server.js
@@ -50,6 +50,8 @@ app.post("/webhook", async function (request, response) {
         msgText = await chat.chatGptService.categorize(messageTimeStamp, messageContent)
         chat.text.send(ourNumberId, messageFrom, msgText);
       } else if(messageType == "audio"){
+        let mediaId = request.body.entry[0].changes[0].value.messages[0].audio.id;
+        let messageContent = await media.mediaService.getFileAndTranscribe(mediaId)
         msgText = await chat.chatGptService.categorize(messageTimeStamp, messageContent);
         chat.text.send(ourNumberId, messageFrom, msgText);
       } else {
