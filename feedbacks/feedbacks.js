af408f733ab593213a5bfb97f5da27c997017a9f
Rafael-daS-Costa <rafaelsilvacosta@id.uff.br>
2023-12-17 15:03:25 -0300

    Ultimas alterações na mensagem retornada ao usuário

--- feedbacks/feedbacks.js
+++ feedbacks/feedbacks.js
@@ -17,7 +17,6 @@ async function getFeedbackMessage(jsonData) {
     switch(jsonData.category){
         case consts.categories.FOOD:  
             msg = `
-Sua mensagem foi: '${jsonData.message}'. 
 
 *${chatGptFeedbackMessage}* 
 
@@ -29,7 +28,6 @@ ${jsonData.items?.length > 0 ? `Itens: ${jsonData.items.join(", ")}`:``}
 
         case consts.categories.MEDICINE:
             msg = `
-Sua mensagem foi: '${jsonData.message}'. 
 
 *${chatGptFeedbackMessage}*
 
@@ -42,7 +40,6 @@ ${jsonData.quantity ? `Quantidade: ${jsonData.quantity} unidades`:``}
 
         case consts.categories.EXERCISE:
             msg = `
-Sua mensagem foi: '${jsonData.message}'. 
 
 *${chatGptFeedbackMessage}* 
 
@@ -56,7 +53,6 @@ ${jsonData.time ? `Tempo: ${jsonData.time}`:``}
         
         case consts.categories.GLUCOSE:
             msg = `
-Sua mensagem foi: '${jsonData.message}'. 
 
 *${chatGptFeedbackMessage}*
 
@@ -68,7 +64,6 @@ ${jsonData.glucose ? `Índice glicêmico: ${jsonData.glucose}mg/dL`:``}
 
         default:
             msg = `
-Sua mensagem foi: '${jsonData.message}'. 
 
 Parece que sua mensagem está fora do contexto de saúde e diabetes. Por favor, envie uma mensagem relacionada a este tema e ficarei feliz em ajudá-lo :)
 `
