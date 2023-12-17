417a580e69041b497d88f8a926dcc0d21a2ab7a0
Beatriz Cardoso <beatriz.cardoso@calindra.com.br>
2023-12-17 14:57:19 -0300

    feat: ajuste data e hora

--- utils/converTime.js
+++ utils/converTime.js
@@ -1,9 +1,9 @@
 function epochToDate(epochTimeStamp) {
     var d = new Date(epochTimeStamp * 1000);
     var year = d.getFullYear();
-    var month = d.getMonth();
+    var month = d.getMonth() + 1;
     var date = d.getDate();
-    var hour = d.getHours();
+    var hour = d.getHours() - 3;
     var min = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
     var sec = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
     var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec ;
